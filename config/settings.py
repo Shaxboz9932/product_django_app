0eb4ab3689c283e2d9d1f6f63e0fe050edbd8054
Shaxboz Azamatov <128015668+Shaxboz9932@users.noreply.github.com>
2025-11-30 21:03:48 +0500

    changed ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["product-django-app.onrender.com", "www.product-django-app.onrender.com"]
 
 
 # Application definition
